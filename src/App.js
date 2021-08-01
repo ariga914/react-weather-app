1cfa09ea0b05c657158c19e05d1d5a65956eceba
ariga914 <82427651+ariga914@users.noreply.github.com>
2021-08-01 19:51:51 +0900

    have implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import "./App.css";
+import Weather from "./components/Weather";
+import FreeSpace from "./components/FreeSpace";
+import { Fragment } from "react";
 
 function App() {
+    const cities = [
+        {
+            city_name: "Tokyo",
+            color_name: "blue",
+        },
+        {
+            city_name: "London",
+            color_name: "yellow",
+        },
+        {
+            city_name: "Paris",
+            color_name: "green",
+        },
+        {
+            city_name: "Los Angeles",
+            color_name: "red",
+        },
+    ];
+
     return (
-        <div className="min-h-screen flex justify-center items-center">
-            Weather Application
+        <div className="m-5 box-shadow">
+            <FreeSpace />
+            <div className="min-h-screen flex justify-center items-center flex-wrap">
+                {cities.map((city, index) => (
+                    <Weather
+                        kyes={index}
+                        city_name={city.city_name}
+                        color_name={city.color_name}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
---
+++ src/components/FreeSpace.js
@@ -0,0 +1,11 @@
+import { React } from "react";
+
+const FreeSpace = () => {
+    return (
+        <div className="border">
+            <h1>Let's attack on TailwindCSS'</h1>
+        </div>
+    );
+};
+
+export default FreeSpace;
